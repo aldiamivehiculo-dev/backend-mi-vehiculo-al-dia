0951592cbdc542245d4125ae797f22c28e39fe7c
aldiamivehiculo-dev <aldiamivehiculo@gmail.com>
2025-12-09 03:55:50 -0300

    Update models.py

--- documentos_vehiculo/models.py
+++ documentos_vehiculo/models.py
@@ -13,6 +13,7 @@ class DocumentoVehicular(models.Model):
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     vehiculo = models.ForeignKey(Vehiculo, on_delete=models.CASCADE, related_name='documentos')
+    archivo = models.FileField(upload_to='documentos/', null=True, blank=True)
     tipo = models.CharField(max_length=2, choices=TIPOS)
     archivo_url = models.URLField(max_length=500, null=True, blank=True)
     fecha_subida = models.DateTimeField(auto_now_add=True)
